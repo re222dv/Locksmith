b55ccde52d80448010324697139213e198ec938a
Rasmus Eneman <rasmus@eneman.eu>
2015-01-22 22:31:59 +0100

    feat: Generate an application key if there are none

--- app/models/locksmith/application.rb
+++ app/models/locksmith/application.rb
@@ -5,5 +5,9 @@ module Locksmith
     validates_presence_of :user
     validates_presence_of :name
     validates_presence_of :key
+
+    after_initialize do
+      self.key = SecureRandom.base64 25 if self.key.nil?
+    end
   end
 end
--- spec/models/locksmith/application_spec.rb
+++ spec/models/locksmith/application_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 module Locksmith
   describe Application do
+    fixtures :locksmith_users
+
     it { should belong_to :user }
 
     it { should validate_presence_of :user }
     it { should validate_presence_of :name }
     it { should validate_presence_of :key }
+
+    it 'should generate a random key when created' do
+      stingray = Application.create name: 'Stingray', user: @anna
+      barracuda = Application.create name: 'Barracuda', user: @anna
+
+      expect(stingray.key.length).to be > 32
+      expect(barracuda.key.length).to be > 32
+
+      expect(stingray.key).not_to be == barracuda.key
+    end
   end
 end
